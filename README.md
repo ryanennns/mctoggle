a55bf3c80f475e997422cbd45f1701a99e1412d1
ryanennns <ryanennns@gmail.com>
2023-10-06 06:37:12 -0400

    cleanup

--- README.md
+++ README.md
@@ -19,7 +19,6 @@ Below is a basic overview of the project structure:
 ├── .env.sample -> sample .env file
 ├── app.js      -> main entrypoint for app
 ├── commands.js -> slash command payloads + helpers
-├── game.js     -> logic specific to RPS
 ├── utils.js    -> utility functions and enums
 ├── package.json
 ├── README.md
--- app.js
+++ app.js
@@ -4,15 +4,9 @@ import {InteractionResponseType, InteractionType,} from 'discord-interactions';
 import {VerifyDiscordRequest} from './utils.js';
 import {disable, enable, status} from "./handlers.js";
 
-// Create an express app
 const app = express();
-// Get port, or default to 3000
 const PORT = process.env.PORT || 3000;
-// Parse request body and verifies incoming requests using discord-interactions package
 app.use(express.json({verify: VerifyDiscordRequest(process.env.PUBLIC_KEY)}));
-
-// Store for in-progress games. In production, you'd want to use a DB
-const activeGames = {};
 
 app.post('/interactions', async function (req, res) {
     // Interaction type and data
